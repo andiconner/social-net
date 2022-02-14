82fe6f6f2c3403a5d60b00c56f154bf62f3a08e0
Andi Conner <andiconner@icloud.com>
2022-02-13 19:00:04 -0600

    user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
 const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    }
+});
+
+// create the user model using the UserSchema
+const User = model('User', UserSchema);
+
+//export the User model
+module.exports = User;
